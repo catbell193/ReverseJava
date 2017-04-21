e84d607ccbb505336903c605c6c2c953ad406fe7
U-CORP\rrowe <rrowe@WIN-UEGN02P4K36.corp.amazonworkspaces.com>
2017-04-21 17:05:24 -0400

    Saved

--- ReverseDemo.java
+++ ReverseDemo.java
@@ -12,22 +12,7 @@ class Reverse {
 			
 		// 	return k;
 	
-    public String reverseR(String str) {
-    	char c[] = str.toCharArray();
-    	int i = str.length()-1;
-    	String j ;
-    	String s;
-    	if( i <= 0) {
-     	return Character.toString(c[0]);
-    	}
-    	else {
-        	s = Character.toString(c[i]);
-        	j = reverseR(s) ;
-        	i--;
-        
-    
-        	return new String(j);
-    	}
+  
 
 	}
 }
